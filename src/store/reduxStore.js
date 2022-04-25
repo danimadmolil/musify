f8db1ee24d31307ca098dccd03deb7d084336667
daniyal <dani.madmolil@gmail.com>
2022-04-25 19:06:24 +0430

    enable tracing in redux

--- src/store/reduxStore.js
+++ src/store/reduxStore.js
@@ -11,7 +11,8 @@ const store = createStore(
   rootReducer(),
   compose(
     applyMiddleware(sagaMiddleWare),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true })
   )
 );
 sagaMiddleWare.run(rootSaga);
