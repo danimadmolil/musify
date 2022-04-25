e9831eddc0973a3f53948ab3caac0229785cc349
daniyal <dani.madmolil@gmail.com>
2022-04-25 19:04:50 +0430

    added customize route to json-server

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const jsonServer = require("json-server");
+const fs = require("fs");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 //todo add custom routes here
-
-// server.get("/custom-route", (req, res) => {
-//   res.jsonp({ id: 1 });
+//customize route to update albums
+// server.get("/updateAlbums", (req, res) => {
+//   // res.jsonp({ id: 1 });
+//   let db = router.db.update("albums", (value) => {
+//     return value.filter((song) => song.id > 5);
+//   });
+//   //db.write(jsonObject) write to database temporary and on server restart it back to default db.json values
+//   let write = router.db.write(db);
+//   //with fs.writeFileSync we write to db.json file directly and the result change to db is Perpetual.
+//   fs.writeFileSync("./db.json", JSON.stringify(write));
+//   res.jsonp({ write });
 // });
 
 server.use(jsonServer.bodyParser);
